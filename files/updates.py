858e811d85c1c3b604fa700d66e205946dce092b
Cheetah26 <scmongo@gmail.com>
2021-04-01 14:32:12 -0400

    Support DMs and cleaner data.ini format

--- files/updates.py
+++ files/updates.py
@@ -1,4 +1,3 @@
-import discord
 import os
 
 # For getting the pdf
@@ -15,11 +14,12 @@ import configparser
 # For loading token
 from dotenv import load_dotenv
 
+# For discord
 from discord.ext import commands, tasks
+import discord
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-POP_PATH = os.getenv("POP_PATH")
 
 config = configparser.ConfigParser()
 config.read('/usr/src/app/data/data.ini')
@@ -42,13 +42,20 @@ async def latest(ctx):
     help="Enable this channel to begin recieving updates"
 )
 async def start(ctx):
-    if isinstance(ctx.author, discord.Member):
+    # Determine where the message was sent
+    if ctx.guild:
+        # Check author's permissions
         if ctx.author.guild_permissions.manage_channels:
             await ctx.send("I will send updates here!")
-            write_config(ctx.channel.id, 'updates', 'yes')
+            write_config('channels', ctx.channel.id, 'yes')
         else:
             await ctx.send("Sorry, you don't have permission to do that")
+    elif ctx.guild == None:
+        # Handle command in DM
+        await ctx.send("Ok, I'll start DM'ing you updates!")
+        write_config('users', ctx.author.id, 'yes')
     else:
+        # Catch-all
         await ctx.send("I'm not sure how to that here")
 
 @bot.command(
@@ -56,13 +63,20 @@ async def start(ctx):
     help="Remove this channel from future updates"
 )
 async def stop(ctx):
+    # Determine where the message was sent
     if isinstance(ctx.author, discord.Member):
+        # Check author's permissions
         if ctx.author.guild_permissions.manage_channels:
             await ctx.send("I'll stop sending updates here")
-            write_config(ctx.channel.id, 'updates', 'no')
+            write_config('channels', ctx.channel.id, 'no')
         else:
             await ctx.send("Sorry, you don't have permission to do that")
+    elif ctx.message.guild == None:
+        # Handle command in DM
+        await ctx.send("I'll stop sending updates here")
+        write_config('users', ctx.author.id, 'no')
     else:
+        # Catch-all
         await ctx.send("I'm not sure how to that here")
 
 # The main runner
@@ -84,33 +98,54 @@ async def send_update():
             print("Checking url: " + url)
             req = requests.get(url)
             if req.status_code == 200:
-                # Save the image
+                # Convert and save the image
                 pages = pdf2image.convert_from_bytes(req.content)
                 pages[0].save('/usr/src/app/data/latest.png', 'PNG')
-                # Send the pdf to all channels
-                activeChannels = await get_channels()
-                print("Sending pdf to channels: ")
-                print_list = []
-                for c in activeChannels:
-                    print_list.append("{}: {}".format(c.guild.name, c.name))
-                print(*print_list, sep = ", ")
-                for channel in activeChannels:
-                    await channel.send(file=discord.File('/usr/src/app/data/latest.png'))
+                # Send the image to all recipients
+                await send_to_channels()
+                await send_to_users()
                 # Update the data file
                 write_config('main', 'lastupdate', today.strftime('%Y-%m-%d'))
             else:
                 print('{}: Request failed with status {}'.format(datetime.now(), req.status_code))
 
+# Channel things
 async def get_channels():
     channels = []
-    for channelID in config.sections():
-        if channelID != 'main':
-            if config.get(channelID, 'updates') == 'yes':
-                channels.append(await bot.fetch_channel(channelID))
+    for channelID in config['channels']:
+        if config['channels'][channelID] == 'yes':
+            channels.append(await bot.fetch_channel(channelID))
     return channels
 
+async def send_to_channels():
+    activeChannels = await get_channels()
+    print("Sending image to channels:\n\t", end="")
+    print_list = []
+    for channel in activeChannels:
+        print_list.append("{}: {}".format(channel.guild.name, channel.name))
+        await channel.send(file=discord.File('/usr/src/app/data/latest.png'))
+    print(*print_list, sep = ", ")
+
+# User things
+async def get_users():
+    users = []
+    for userID in config['users']:
+        if config['users'][userID] == 'yes':
+            users.append(await bot.fetch_user(userID))
+    return users
+
+async def send_to_users():
+    activeUsers = await get_users()
+    print("Sending image to users:\n\t", end="")
+    print_list = []
+    for user in activeUsers:
+        print_list.append(user.name)
+        await user.send(file=discord.File('/usr/src/app/data/latest.png'))
+    print(*print_list, sep=',')
+
+# Everything things
 def write_config(section, field, data):
-    config[section] = {field: data}
+    config.set(str(section), str(field), str(data))
     with open('/usr/src/app/data/data.ini', 'w') as configfile:
         config.write(configfile)
 
